b452d7bc6a777c7fafc9aa1816e471fc090ca423
HoangNN <hoangdragons@gmail.com>
2018-10-14 20:43:14 +0700

    add model file

---
+++ TestOnline/TestOnline/Models/AnswerModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestOnline.Models
+{
+    public class AnswerModel
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+        public int questionID { get; set; }
+        public bool isAnswering { get; set; }
+    }
+}
---
+++ TestOnline/TestOnline/Models/QuestionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestOnline.Models
+{
+    public class QuestionModel
+    {
+        public int mabailam { get; set; }
+        public int id { get; set; }
+        public string content { get; set; }
+        public List<AnswerModel> listAnswer { get; set; }
+    }
+}
